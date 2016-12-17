dce603ec1ef88f7454c926b27c8cd92bab3cbffe
Linuxea <Linuxea@foxmail.com>
2016-12-17 16:42:13 +0800

    增加项目文档目录，有来统一存放相关信息

--- wozi/src/main/webapp/项目文档/sql/tbwoziuser.sql
+++ wozi/src/main/webapp/项目文档/sql/tbwoziuser.sql
@@ -18,3 +18,22 @@ CHANGE COLUMN `registdate` `registdate` DATETIME NULL DEFAULT NULL ,
 CHANGE COLUMN `lastlogindate` `lastlogindate` DATETIME NULL DEFAULT NULL ;
 /*   alert user table end   */
 /* 2016-12-16 end   */
+
+/* 2016-12-17 begin */
+table name : "tb_wozi_note_menu",
+字段:id   parent   text(默认表示名称)  flag（0表示目录，1表示文件) ref_user(关联用户)
+create table tb_wozi_note_menu (
+	id varchar(36) primary key,
+	parent varchar(36) default "#",
+	text varchar(36),
+	flag varchar(1) default 0,
+    ref_user varchar(36) ,
+    constraint foreign key (ref_user) references tbwoziuser(id) ON delete cascade
+    )engine=innoDB default charset=utf8
+/* 2016-12-17 end */
+
+
+
+
+
+
